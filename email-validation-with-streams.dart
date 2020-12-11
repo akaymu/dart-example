4a667c67b36eac96542def9508ba79d914aa85ac
Muhlis Murat Akay <akaymu@gmail.com>
2020-12-11 14:13:25 +0300

    Create email-validation-with-streams.dart

---
+++ email-validation-with-streams.dart
@@ -0,0 +1,31 @@
+// <h4>Enter Your Email!</h4>
+// <input />
+// <div style="color: red;"></div>
+
+import 'dart:html';
+import 'dart:async';
+
+void main() {
+  final InputElement input = querySelector('input');
+  final DivElement div = querySelector('div');
+  
+  final validator = new StreamTransformer.fromHandlers(
+    handleData: (inputValue, sink) {
+      if(inputValue.contains('@')) {
+        sink.add(inputValue);
+      } else {
+        sink.addError('Enter a valid email');
+      }
+    }
+  );
+  
+  input.onInput
+    .map((dynamic event) => event.target.value)
+    .transform(validator)
+    .listen(
+      (inputValue) => div.innerHtml = '',
+    onError: (err) => div.innerHtml = err
+    );
+
+    
+}
