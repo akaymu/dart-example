4dde916e481d0f2c1698454c1396d540e06f64a3
Muhlis Murat Akay <akaymu@gmail.com>
2020-12-11 17:16:48 +0300

    Update using-getter-in-bloc.class.dart

--- using-getter-in-bloc.class.dart
+++ using-getter-in-bloc.class.dart
@@ -3,22 +3,23 @@ import 'dart:async';
 void main() {
   final bloc = new Bloc();
   
-  // bloc.emailController.stream.listen((value) {
-  //   print(value);
-  // });
   bloc.email.listen((value) {
     print(value);
   });
   
   
-  // bloc.emailController.sink.add('My New EMAIL');
   bloc.changeEmail('My New EMAIL');
 }
 
 class Bloc {
-  final emailController = StreamController<String>();
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
   
-  get changeEmail => emailController.sink.add;
+  // Add data to stream
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
   
-  get email => emailController.stream;
+  // Retrieve data from stream
+  Stream<String> get email => _emailController.stream;
+  Stream<String> get password => _passwordController.stream;
 }
