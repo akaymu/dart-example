3ae8a4fcc8881a097516f889940988752d190132
Muhlis Murat Akay <akaymu@gmail.com>
2021-01-28 12:07:15 +0300

    Update streams.dart

--- streams.dart
+++ streams.dart
@@ -1,34 +1,34 @@
 import 'dart:async';
 
-class Cake {} // Possible output
+class Cake {}
 
-class Order { // Input
-  final String type;
+class Order {
+  String type;
   Order(this.type);
 }
 
-void main() {
+void main () {
   final controller = new StreamController();
-  
   final order = new Order('chocolate');
   
+  // Bu kısım BAKER'ın yaptığı işi temsil eder.
+  // type'ı alır ve karar verip Cake ya da Error çıkarır.
   final baker = new StreamTransformer.fromHandlers(
     handleData: (cakeType, sink) {
-      if (cakeType == 'chocolate') {
+      if(cakeType == 'chocolate') {
         sink.add(new Cake());
       } else {
-        sink.addError('I cant bake that type!');
+        sink.addError('I can not bake that type!');
       }
     }
   );
   
-  controller.sink.add(order); // Order Taker
+  controller.sink.add(order); // Bu kısım Order Taker'ı temsil eder.
   
-  controller.stream // Chocolate Cake Factory
-    .map((order) => order.type) // Order Inspector
-    .transform(baker) // Baker - it is stream transformer
-    .listen( // Pickup Office
-      (cake) => print('Heres your cake $cake'),
-      onError: (err) => print(err),
-    );
+  controller.stream
+    .map((order) => order.type) // Bu kısım Order Inspector'u temsil eder.
+    .transform(baker) // Bu kısım baker'ı temsil eder.
+    .listen( // Bu kısım Pickup Office'i temsil eder.
+    (cake) => print('Here is your cake $cake'), 
+    onError: (err) => print(err));
 }
